2cc82e349a185911e805178caac99261385ab295
Eugeniy Buzyuma <byzuma.e.a@gmail.com>
2014-10-30 23:59:40 +0300

    Divide work with server and work with android view

--- ChatClient/Chat.Android/MainActivity.cs
+++ ChatClient/Chat.Android/MainActivity.cs
@@ -11,106 +11,45 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Chat.Core;
 
-namespace Chat.Android
+namespace Chat.Droid
 {
-	[Activity (Label = "Chat Android", MainLauncher = true, Icon = "@drawable/icon")]
+	[Activity (Label = "Chat", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		private string _serverUrl = "http://172.24.80.54:31337/";
-		private string _getUrlTemplate = "{0}?token={1}";
-		//private string _serverUrl = "http://10.163.0.135:31337/";
-
-		private int _token = 0;
-		private ArrayAdapter _listAdapter;
-
-
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 			SetContentView (Resource.Layout.Main);
 
+			var serverHelper = new ServerHelper (PopulateView);
+
 			Button button = FindViewById<Button> (Resource.Id.SendButton);			
-			button.Click += delegate {
+			button.Click += (object sender, EventArgs e) => 
+			{
 				EditText message = FindViewById<EditText> (Resource.Id.MessageField);			
 				if (!String.IsNullOrWhiteSpace(message.Text))
 				{
-					Post(message.Text);
+					serverHelper.Post(message.Text);
 					message.Text = String.Empty;
 				}
 			};
 
 
-			// Add listadapter for view
-			_listAdapter = new ArrayAdapter<string> (this, Android.Resource.Layout.ListItemTemplate);
-
+			// Initialize list adapter
 			ListView list = FindViewById<ListView> (Resource.Id.MessagesList);
-			list.Adapter = _listAdapter;
+			list.Adapter = new ArrayAdapter<string> (this, Android.Resource.Layout.SimpleListItem1);
 
-			Get();
+			serverHelper.Get();
 		}
 
-		public void Get ()
+		public void PopulateView(List<String> messages)
 		{
-			string url = String.Format(_getUrlTemplate, _serverUrl, _token);
-
-			WebRequest httpReq = WebRequest.Create (url);
-			httpReq.BeginGetResponse (ResponseGetCallback, httpReq);
-		}
-
-		private void ResponseGetCallback (IAsyncResult ar)
-		{
-			var httpReq = (WebRequest) ar.AsyncState;
-
-			using (WebResponse httpRes = httpReq.EndGetResponse (ar)) {
-				ParseResults (httpRes);
-			}
-
-			Get ();
-		}
-
-		private void ParseResults (WebResponse httpRes)
-		{
-			JsonObject jsonObject = (JsonObject)JsonObject.Load (httpRes.GetResponseStream());
-			List<string> incomingMessages = ((JsonArray)jsonObject ["messages"]).Select(p => (string)p).ToList();
-
-			int incomingToken = (int)jsonObject["token"];
-
-			if (incomingToken > _token) 
-			{
-				_token = incomingToken;
-				RunOnUiThread (() => _listAdapter.AddAll (incomingMessages));
-			}
-		}
-
-		private void Post(string message)
-		{
-			var request = WebRequest.Create (_serverUrl);
-			request.Method = "POST";
-			using (var stream = request.GetRequestStream ()) 
-			{
-				using (var streamWriter = new StreamWriter (stream)) 
-				{
-					streamWriter.Write ("\"" + message + "\"");
-					streamWriter.Flush ();
-
-					_token++;
-					_listAdapter.Add (message);
-
-					request.BeginGetResponse (ResponsePostCallback, request);
-
-
-				}
-			}
-		}
-
-		private void ResponsePostCallback (IAsyncResult ar)
-		{
-			var httpReq = (WebRequest) ar.AsyncState;
-
-			using (WebResponse httpRes = httpReq.EndGetResponse (ar)) {
-			}
-
+			RunOnUiThread (() => {
+				ListView list = FindViewById<ListView> (Resource.Id.MessagesList);
+				((ArrayAdapter)list.Adapter).AddAll (messages);
+			});
 		}
 			
 	}
---
+++ ChatClient/Chat.Android/ServerHelper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net;
+using System.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chat.Core
+{
+	public class ServerHelper
+	{
+		private readonly string _serverUrl = "http://172.24.80.54:31337/";
+		private readonly string _getUrlTemplate = "{0}?token={1}";
+
+		private int _token;
+
+		private Action<List<string>> _populateView;
+
+		public ServerHelper (Action<List<string>> populateView)
+		{
+			_token = 0;
+			_populateView = populateView;
+		}
+
+		public void Get ()
+		{
+			string url = String.Format(_getUrlTemplate, _serverUrl, _token);
+
+			WebRequest httpReq = WebRequest.Create (url);
+			httpReq.BeginGetResponse (ResponseGetCallback, httpReq);
+		}
+
+		private void ResponseGetCallback (IAsyncResult ar)
+		{
+			var httpReq = (WebRequest) ar.AsyncState;
+
+			using (WebResponse httpRes = httpReq.EndGetResponse (ar)) {
+				ParseResults (httpRes);
+			}
+
+			Get ();
+		}
+
+		private void ParseResults (WebResponse httpRes)
+		{
+			JsonObject jsonObject = (JsonObject)JsonObject.Load (httpRes.GetResponseStream());
+			List<string> incomingMessages = ((JsonArray)jsonObject ["messages"]).Select(p => (string)p).ToList();
+
+			int incomingToken = (int)jsonObject["token"];
+
+			if (incomingToken > _token) 
+			{
+				_token = incomingToken;
+
+				_populateView(incomingMessages);
+			}
+		}
+
+		public void Post(string message)
+		{
+			var request = WebRequest.Create (_serverUrl);
+			request.Method = "POST";
+			using (var stream = request.GetRequestStream ()) 
+			{
+				var jsonObj = new JsonPrimitive (message);
+				jsonObj.Save (stream);
+
+				_token++;
+				_populateView (new List<string>{message});
+
+				request.BeginGetResponse (ResponsePostCallback, request);
+			}
+		}
+
+		private void ResponsePostCallback (IAsyncResult ar)
+		{
+			var httpReq = (WebRequest) ar.AsyncState;
+
+			using (WebResponse httpRes = httpReq.EndGetResponse (ar))
+			{
+
+			}
+
+		}
+
+	}
+}
+
